ed7b4363971ca1025fb865f8fc8190aacc58b0f0
rimalon <rimalon@yandex.ru>
2020-11-30 00:08:58 +0300

    Add cfg generation

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# prime_numbers_grammars
+# Prime numbers generator
+##Usage
+#####For context free grammar generating use:
+```
+python cfg_generator.py
+```
+From "/turing_machine_generator" folder
+#####To generate context free grammar with specific arguments use:
+```
+usage: cfg_generator.py [-h] [-tm TURING_MACHINE_PATH] [-g GRAMMAR_PATH]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -tm TURING_MACHINE_PATH, --turing_machine_path TURING_MACHINE_PATH
+                        Path to turing machine file
+  -g GRAMMAR_PATH, --grammar_path GRAMMAR_PATH
+                        Output grammar path
+```
+#####For prime numbers generating use:
+```
+python prime_generator.py
+```
+From "/turing_machine_generator" folder
+#####To start the prime number generator with specific arguments use:
+```
+usage: prime_generator.py [-h] [-g GRAMMAR_PATH] [-n N]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -g GRAMMAR_PATH, --grammar_path GRAMMAR_PATH
+                        Path to file with grammar
+  -n N                  Prime numbers amount
+```
---
+++ turing_machine_generator/prime_generator.py
@@ -0,0 +1,50 @@
+import argparse
+from collections import deque
+
+
+def word_generator(productions, init_symbol_1, init_symbol_2, input_symbol):
+    q = deque([init_symbol_1])
+    st = set()
+    while len(q):
+        word = q.popleft()
+        if word not in st:
+            st.add(word)
+            if all(c == input_symbol for c in word):
+                yield word
+            else:
+                for left, right in productions:
+                    if left in word:
+                        new_word = word.replace(left, right)
+                        if any(S in new_word for S in [init_symbol_1, init_symbol_2]):
+                            q.append(new_word)
+                        else:
+                            q.appendleft(new_word)
+
+
+def read_free_grammar(path):
+    grammar = open(path)
+    str_productions = [line.strip('\n') for line in grammar.readlines()]
+    productions = []
+    for line in str_productions:
+        line = line.split(' -> ')
+        productions += [tuple(line)] if len(line) > 1 else [(line[0], '')]
+    grammar.close()
+    return productions
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--grammar_path", help="Path to file with grammar",
+                        type=str, default="./cfg_prime_grammar.txt")
+    parser.add_argument("-n", help="Prime numbers amount", default=10, type=int)
+    args = parser.parse_args()
+
+    productions = read_free_grammar(args.grammar_path)
+    gen = word_generator(productions, 'First', 'Second', 'I')
+
+    for i in range(args.n):
+        print(len(gen.__next__()))
+
+
+if __name__ == '__main__':
+    main()
